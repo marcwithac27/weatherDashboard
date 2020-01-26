2525865d721025093ea182605d33c6b1bc8b8d3e
Marc Maldonado <marc@telecomelite.com>
2020-01-26 09:27:43 -0500

    got the search to work

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 var date = moment().format('l')
 var time = moment().format('LTS')
 $('.date').append(date)
-var search = 'Sanford'
+
+
+$('button').click(function(){
+var search = $('#search').val()
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ search +"&appid=421493e1a9d6d7ce1b656ee17274f671";
+
+
+
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-      var city = response.name
-      var temp = response.main.temp
-      var humidity = response.main.humidity
+      console.log(response)
+    var city = response.name
+    var temp = response.main.temp
+    var humidity = response.main.humidity
       
-      $('button').click(function(){
-    
+          
         $('.city').append(city)
         $('.temp').append('Temperture: ', temp)
         $('#humidity').append('Humidity: ', humidity)
-      console.log(response)
+      
           
       })
       
-    });
+    })
     
   
