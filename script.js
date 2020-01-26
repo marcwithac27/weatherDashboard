2e62e7a8665bbf54a30274c5b4b5acd5d80e82d3
Marc Maldonado <marc@telecomelite.com>
2020-01-26 11:56:44 -0500

    UV Index works on local storge

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
 var search = localStorage.getItem('savedCity')
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ search +"&units=imperial&appid=421493e1a9d6d7ce1b656ee17274f671";
+var wApi = "421493e1a9d6d7ce1b656ee17274f671"
+var queryURLCity = "https://api.openweathermap.org/data/2.5/weather?q="+ search +"&units=imperial&appid=" + wApi;
+
 var date = moment().format('l')
 var time = moment().format('LTS')
 $('.date').append(date)
 $.ajax({
-    url: queryURL,
+    url: queryURLCity,
     method: "GET"
   }).then(function(response) {
-      console.log(response)
+      //console.log(response)
     var city = response.name
     var temp = response.main.temp
     var humidity = response.main.humidity
     var windSpeed = response.wind.speed
+    var lat = response.coord.lat
+    var lon = response.coord.lon
         $('.city').append(city)
         $('.temp').append('Temperture: ', temp, ' °F')
         $('#humidity').append('Humidity: ', humidity)
         $('#windSpeed').append('Wind Speed ', windSpeed)
-          
+         //console.log(lat, lon) 
+         var queryURLUvIndex = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + wApi;
+         $.ajax({
+            url: queryURLUvIndex,
+            method: "GET"
+          }).then(function(response){
+            $('#uvIndex').append('UV Index ', response.value)
+          })
       })
 
 
 $('button').click(function(){
 var search = $('#search').val()
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ search +"&units=imperial&appid=421493e1a9d6d7ce1b656ee17274f671";
+var queryURLCity = "https://api.openweathermap.org/data/2.5/weather?q="+ search +"&units=imperial&appid=421493e1a9d6d7ce1b656ee17274f671";
 $('.city').empty()
 $('.temp').empty()
 $('#humidity').empty()
 localStorage.setItem('savedCity', search)
 $.ajax({
-    url: queryURL,
+    url: queryURLCity,
     method: "GET"
   }).then(function(response) {
       console.log(response)
@@ -40,7 +51,7 @@ $.ajax({
         $('.temp').append('Temperture: ', temp, ' °F')
         $('#humidity').append('Humidity: ', humidity)
         $('#windSpeed').append('Wind Speed ', windSpeed)
-          
+        
       })
       
     })
