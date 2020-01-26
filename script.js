4a0c35fedf6cf82afd4cf3f4358baa1b7c171f5a
Marc Maldonado <marc@telecomelite.com>
2020-01-26 10:36:01 -0500

    got local storage done

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
+var search = localStorage.getItem('savedCity')
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ search +"&units=imperial&appid=421493e1a9d6d7ce1b656ee17274f671";
 var date = moment().format('l')
 var time = moment().format('LTS')
 $('.date').append(date)
-
-
-$('button').click(function(){
-var search = $('#search').val()
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ search +"&appid=421493e1a9d6d7ce1b656ee17274f671";
-
-
-
 $.ajax({
     url: queryURL,
     method: "GET"
@@ -17,12 +11,35 @@ $.ajax({
     var city = response.name
     var temp = response.main.temp
     var humidity = response.main.humidity
-      
-          
+    var windSpeed = response.wind.speed
         $('.city').append(city)
-        $('.temp').append('Temperture: ', temp)
+        $('.temp').append('Temperture: ', temp, ' °F')
         $('#humidity').append('Humidity: ', humidity)
-      
+        $('#windSpeed').append('Wind Speed ', windSpeed)
+          
+      })
+
+
+$('button').click(function(){
+var search = $('#search').val()
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ search +"&units=imperial&appid=421493e1a9d6d7ce1b656ee17274f671";
+$('.city').empty()
+$('.temp').empty()
+$('#humidity').empty()
+localStorage.setItem('savedCity', search)
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response)
+    var city = response.name
+    var temp = response.main.temp
+    var humidity = response.main.humidity
+    var windSpeed = response.wind.speed
+        $('.city').append(city)
+        $('.temp').append('Temperture: ', temp, ' °F')
+        $('#humidity').append('Humidity: ', humidity)
+        $('#windSpeed').append('Wind Speed ', windSpeed)
           
       })
       
